fa50183a4e96ed2c949fd74acc38a8a74ff78b92
Dave Anonuevo <47778370+daveanonuevo@users.noreply.github.com>
2019-02-24 10:44:06 +0800

    Create HelloWorld.cs
    Hello world in C# xdd

---
+++ HelloWorld.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace HelloWorld
+{
+    class xdd
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
